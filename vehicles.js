d498edf28f8001c51deca616ebda2e5bd03a2db8
CathyVisagie <134547360+CathyVisagie@users.noreply.github.com>
2023-06-14 22:32:52 +0200

    Create vehicles.js

---
+++ vehicles.js
@@ -0,0 +1,5 @@
+const vehicles = ["ford", "honda", "nissan", "renault", "hyundai"]; 
+for (let i = 0; i < vehicles.length; i++){
+// list the cars I would like to drive 
+console.log(`I would love to drive a ${(vehicles[i])}`);
+}
